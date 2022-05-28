c6303ffda4082813a837816ea4be2fcc0fe399ed
lixin <1605670114@qq.com>
2022-05-28 21:31:46 +0800

    code specification.

--- src/main/java/com/lixin/campusforum/ServletInitializer.java
+++ src/main/java/com/lixin/campusforum/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.lixin.campusforum;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author lixin
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
